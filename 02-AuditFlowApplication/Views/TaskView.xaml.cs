234b038dfbe69fab3e5348012fb83aa923224623
caitlyn_stirling <caitlynstirling@aol.com>
2026-01-25 15:07:17 +0000

    Added in design layout for Audit and Task Page and added new resources in.

--- 02-AuditFlowApplication/Views/TaskView.xaml.cs
+++ 02-AuditFlowApplication/Views/TaskView.xaml.cs
@@ -7,25 +7,11 @@ namespace _02_AuditFlowApplication.Views
 {
     public partial class TaskView : UserControl
     {
-        private DateTime currentMonth;
-        private Dictionary<DateTime, List<string>> auditEvents;
 
         public TaskView()
         {
             InitializeComponent();
   
-            InitializeAuditEvents();
-        }
-
-        private void InitializeAuditEvents()
-        {
-            // Initialize sample audit events to match wireframe
-            auditEvents = new Dictionary<DateTime, List<string>>
-            {
-                { new DateTime(2026, 10, 2), new List<string> { "Risk Compliance" } },
-                { new DateTime(2026, 10, 13), new List<string> { "Supplier Assess" } },
-                { new DateTime(2026, 10, 22), new List<string> { "IT Systems Audit" } }
-            };
         }
 
         private void DashboardButton_Click(object sender, RoutedEventArgs e)
